7edb08aa8e8da1e14ed34831e342e30af45ca881
Alec Prettyman <prettale4@gmail.com>
2022-05-13 15:00:18 -0600

    Updated how languages are stored

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
+let languages = [
+  {
+    name: 'Javascript',
+    img: ''
+  },
+  {
+    name: 'React',
+    img: ''
+  },
+  {
+    name: 'Redux',
+    img: ''
+  },
+  {
+    name: 'Axios',
+    img: ''
+  },
+  {
+    name: 'HTML',
+    img: ''
+  },
+  {
+    name: 'CSS',
+    img: ''
+  },
+  {
+    name: 'D3',
+    img: ''
+  },
+  {
+    name: 'Node',
+    img: ''
+  },
+  {
+    name: 'Express',
+    img: ''
+  },
+  {
+    name: 'PostgreSQL',
+    img: ''
+  }
+]
+
+const languagesDisplay = document.getElementById('languages-display')
+
+languagesDisplay.innerHTML = ''
+
+languages.forEach(language => {
+  const languageDisp = document.createElement('div')
+  languageDisp.innerHTML = `
+  <p>${language.name}</p>`
+
+  languagesDisplay.appendChild(languageDisp)
+})
+
 const navButtons = document.querySelectorAll('nav button')
 
 const scrollToTag = (element) => {
@@ -18,4 +73,11 @@ navButtons.forEach(button => {
 
     scrollToTag(button.value)
   })
+})
+
+const brokenSite = document.getElementById('broken-site')
+
+brokenSite.addEventListener('click', (event) => {
+  event.preventDefault()
+  alert(`I'm very sorry, but with some recent security updates I did, the site is no longer functioning. I am working hard to get it back up and running. Please refer to the Github repo if you want a look at what I've been doing!`)
 })
