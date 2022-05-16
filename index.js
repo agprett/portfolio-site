a8676f9c2a9a02130d38371be628c76fcff2c908
Alec Prettyman <prettale4@gmail.com>
2022-05-15 23:42:36 -0600

    added experience and education section info

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 let languages = [
   {
     name: 'Javascript',
-    img: ''
+    img: '<i class="devicon-javascript-plain colored language-pic"></i>'
   },
   {
     name: 'React',
-    img: ''
+    img: '<i class="devicon-react-original colored language-pic"></i>'
   },
   {
     name: 'Redux',
-    img: ''
-  },
-  {
-    name: 'Axios',
-    img: ''
+    img: '<i class="devicon-redux-original colored language-pic"></i>'
   },
   {
     name: 'HTML',
-    img: ''
+    img: '<i class="devicon-html5-plain colored language-pic"></i>'
   },
   {
     name: 'CSS',
-    img: ''
+    img: '<i class="devicon-css3-plain colored language-pic"></i>'
   },
   {
     name: 'D3',
-    img: ''
+    img: '<i class="devicon-d3js-plain colored language-pic"></i>'
   },
   {
     name: 'Node',
-    img: ''
-  },
-  {
-    name: 'Express',
-    img: ''
+    img: '<i class="devicon-nodejs-plain colored language-pic"></i>'
   },
   {
     name: 'PostgreSQL',
-    img: ''
+    img: '<i class="devicon-postgresql-plain colored language-pic"></i>'
   }
 ]
 
@@ -47,10 +39,114 @@ languagesDisplay.innerHTML = ''
 
 languages.forEach(language => {
   const languageDisp = document.createElement('div')
+
+  languageDisp.classList.add('language-display')
+
   languageDisp.innerHTML = `
+  ${language.img}
   <p>${language.name}</p>`
 
   languagesDisplay.appendChild(languageDisp)
+})
+
+let education = [
+  {
+    school: 'BYU-Idaho',
+    graduated: 'In Progress',
+    award: 'Computer Science Degree',
+    addedNotes: []
+  },
+  {
+    school: 'Devmountain',
+    graduated: 'May, 2020',
+    award: 'Full Stack Web Development Certificate',
+    addedNotes: ['Transitioned mid-course to a fully remote learning environment', 'Solo and group work experience']
+  }
+]
+
+const educationSection = document.getElementById('education')
+
+education.forEach(edu => {
+  const eduDisplay = document.createElement('div')
+
+  eduDisplay.classList.add('education-display')
+
+  const h3 = document.createElement('h3')
+  h3.innerHTML = `${edu.school}, Graduated: ${edu.graduated}`
+
+  const p = document.createElement('p')
+  p.classList.add('education-award')
+  p.innerHTML = edu.award
+
+  const ul = document.createElement('ul')
+
+  edu.addedNotes.forEach(note => {
+    const li = document.createElement('li')
+
+    li.innerHTML = note
+
+    ul.appendChild(li)
+  })
+
+  eduDisplay.appendChild(h3)
+  eduDisplay.appendChild(p)
+  eduDisplay.appendChild(ul)
+
+  
+  educationSection.appendChild(eduDisplay)
+  
+  eduDisplay.style.height = `${eduDisplay.clientHeight + 10}px`
+})
+
+let experience = [
+  {
+    position: 'Foundations Mentor',
+    company: 'Devmountain',
+    start: 'November 2021',
+    end: 'Present',
+    descriptions: ['Teach new programmers the foundational principles of coding', 'Work with instructors and curriculum planners to ensure students are taught foundational principles in the most efficient and successful way', 'Plan and provide additional support for students as they learn this new skill set']
+  },
+  {
+    position: 'Skilled Facilities Maintenance Technician',
+    company: 'Walmart',
+    start: 'December 2020',
+    end: 'October 2021',
+    descriptions: ['Maintain, diagnose issues, and perform maintenance on facilities, fixtures and equipment', 'Utilize knowledge in multiple craft skills including: plumbing, electrical, carpentry, material handling equipment, and food equipment', 'Manage work orders and routine maintenance schedules by completing and providing required written and electronic information']
+  }
+]
+
+const experienceSection = document.getElementById('experience')
+
+experience.forEach(exp => {
+  const expDisplay = document.createElement('div')
+
+  expDisplay.classList.add('education-display')
+
+  const h3 = document.createElement('h3')
+  h3.innerHTML = exp.position
+
+  const p = document.createElement('p')
+  p.classList.add('education-award')
+  p.innerHTML = `${exp.company}, ${exp.start} - ${exp.end}`
+
+  const ul = document.createElement('ul')
+
+  exp.descriptions.forEach(desc => {
+    const li = document.createElement('li')
+
+    li.innerHTML = desc
+
+    ul.appendChild(li)
+  })
+
+  expDisplay.appendChild(h3)
+  expDisplay.appendChild(p)
+  expDisplay.appendChild(ul)
+
+  
+  experienceSection.appendChild(expDisplay)
+  
+  expDisplay.style.height = `${expDisplay.clientHeight + 10}px`
 })
 
 const navButtons = document.querySelectorAll('nav button')
